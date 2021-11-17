0e92e5127113eb1ddbe0e3c43b4b8af5f54f3148
Tom Harper <tomb1n0@outlook.com>
2021-11-17 19:15:53 +0000

    wip

--- build.sh
+++ build.sh
@@ -1,37 +1,29 @@
 #!/bin/bash
+# Use this when wanting to manually build outside of CI.
 
-# nginx
-(cd nginx && docker build -f DockerFile . -t rareloop/volker:nginx && docker push rareloop/volker:nginx)
+PLATFORMS='linux/amd64,linux/arm64'
+IMAGES=(
+    'nginx'
+    'nginx-cors'
+    'php-71'
+    'php-72'
+    'php-73'
+    'php-74'
+    'php-80'
+    'node-6'
+    'node-8'
+    'node-10'
+    'node-11'
+    'nvm'
+)
 
-# nginx with cors support
-(cd nginx-cors && docker build -f DockerFile . -t rareloop/volker:nginx-cors && docker push rareloop/volker:nginx-cors)
+docker buildx rm volkerbuilder > /dev/null 2>&1 
+docker buildx create --use --name=volkerbuilder > /dev/null 2>&1 
 
-# php-71
-(cd php-71 && docker build -f DockerFile . -t rareloop/volker:php-71 && docker push rareloop/volker:php-71)
+# Build the images
+for IMAGE in ${IMAGES[@]}; do
+    (cd ${IMAGE} && docker buildx build -f DockerFile . -t rareloop/volker:${IMAGE} --platform ${PLATFORMS} --push)
+done
 
-# php-72
-(cd php-72 && docker build -f DockerFile . -t rareloop/volker:php-72 && docker push rareloop/volker:php-72)
-
-# php-73
-(cd php-73 && docker build -f DockerFile . -t rareloop/volker:php-73 && docker push rareloop/volker:php-73)
-
-# php-74
-(cd php-74 && docker build -f DockerFile . -t rareloop/volker:php-74 && docker push rareloop/volker:php-74)
-
-# php-80
-(cd php-80 && docker build -f DockerFile . -t rareloop/volker:php-80 && docker push rareloop/volker:php-80)
-
-# node-6
-(cd node-6 && docker build -f DockerFile . -t rareloop/volker:node-6 && docker push rareloop/volker:node-6)
-
-# node-8
-(cd node-8 && docker build -f DockerFile . -t rareloop/volker:node-8 && docker push rareloop/volker:node-8)
-
-# node-10
-(cd node-10 && docker build -f DockerFile . -t rareloop/volker:node-10 && docker push rareloop/volker:node-10)
-
-# node-11
-(cd node-11 && docker build -f DockerFile . -t rareloop/volker:node-11 && docker push rareloop/volker:node-11)
-
-# nvm
-(cd nvm && docker build -f DockerFile . -t rareloop/volker:nvm && docker push rareloop/volker:nvm)
+# Cleanup
+docker buildx rm volkerbuilder
