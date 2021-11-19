8fb980909401b34a6de24cc4c5ea35f8f344db11
Tom Harper <tom.harper@rareloop.com>
2021-11-19 14:29:05 +0000

    Add ability to install extra PHP extensions via a space seperated environment variable

--- resources/scripts/php/root-start-script.sh
+++ resources/scripts/php/root-start-script.sh
@@ -4,10 +4,15 @@ echo "Running Root startup script";
 
 echo "Running as: $(whoami)"
 
-DEFAULT_BRANCH="1"
-
-COMPOSER_BRANCH="${COMPOSER_BRANCH:=${DEFAULT_BRANCH}}"
-
 # Install composer
+DEFAULT_COMPOSER_BRANCH="1"
+COMPOSER_BRANCH="${COMPOSER_BRANCH:=${DEFAULT_COMPOSER_BRANCH}}"
+
 echo "Installing Composer version \"${COMPOSER_BRANCH}\""
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer --$COMPOSER_BRANCH
+
+# Install additional PHP extensions
+if [ -n "$PHP_EXTENSIONS" ]; then
+    echo "Installing Extra PHP Extensions: ${PHP_EXTENSIONS}"
+    docker-php-ext-install ${PHP_EXTENSIONS}
+fi
