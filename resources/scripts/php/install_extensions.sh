87865733292b6821ab146b03eaa51bdbc0ee97cf
Tom Harper <tom.harper@rareloop.com>
2021-11-19 17:02:44 +0000

    wip

--- resources/scripts/php/install_extensions.sh
+++ resources/scripts/php/install_extensions.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-docker-php-ext-install mysqli pdo_mysql mbstring zip exif pcntl soap
-docker-php-ext-configure gd --with-gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ --with-png-dir=/usr/include/
-docker-php-ext-install zip gd sockets opcache intl
-pecl install redis && docker-php-ext-enable redis
-pecl install imagick && docker-php-ext-enable imagick
-pecl install yaml && docker-php-ext-enable yaml
+install-php-extensions mysqli \
+    pdo_mysql \
+    mbstring \
+    exif \
+    pcntl \
+    soap \
+    gd \
+    zip \
+    sockets \
+    opcache \
+    intl \
+    redis \
+    imagick \
+    yaml
