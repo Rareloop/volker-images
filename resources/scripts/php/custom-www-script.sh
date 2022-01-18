0e9d130b9caaa9a88166aecd93f953f70acce2f5
Ollie Wells <ollie.wells@rareloop.com>
2022-01-18 11:12:51 +0000

    Add custom scripts that can be overridden by projects to do project specific activities on startup

---
+++ resources/scripts/php/custom-www-script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# This script will be run after www-start-script.sh so can be replaced
+# with a bind-mount to run custom operations when an image starts
--- resources/scripts/php/entrypoint.sh
+++ resources/scripts/php/entrypoint.sh
@@ -8,9 +8,19 @@ if [ -f '/docker-scripts/root-start-script.sh' ]; then
     sudo -E /docker-scripts/root-start-script.sh
 fi
 
+if [ -f '/docker-scripts/custom-root-script.sh' ]; then
+    echo 'Found custom-root-script.sh to run'
+    sudo -E /docker-scripts/custom-root-script.sh
+fi
+
 if [ -f '/docker-scripts/www-start-script.sh' ]; then
     echo 'Found www-start-script.sh to run'
     /docker-scripts/www-start-script.sh
+fi
+
+if [ -f '/docker-scripts/custom-www-script.sh' ]; then
+    echo 'Found custom-www-script.sh to run'
+    /docker-scripts/custom-www-script.sh
 fi
 
 echo "Running command exec $*"
