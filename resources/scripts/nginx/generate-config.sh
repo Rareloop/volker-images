9630b85aa749c61c0fe73927a39f742b3a4dd51d
Ollie Wells <ollie.wells@rareloop.com>
2022-06-13 10:42:29 +0100

    Update method for generate config

--- resources/scripts/nginx/generate-config.sh
+++ resources/scripts/nginx/generate-config.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Be explicit about which variables we're expecting
 DOCUMENT_ROOT=${DOCUMENT_ROOT}
@@ -6,67 +6,91 @@ APP_URL=${APP_URL}
 SHARED_URL=${SHARED_URL}
 CORS_ENABLED=${CORS_ENABLED}
 
-if [[ ! -z "${CORS_OVERRIDE}" ]]; then
+if [[ -n "${CORS_OVERRIDE}" ]]; then
     CORS_ENABLED=1
 fi
 
-# Render the template itself
-# This is pretty dumb but i don't know a better way of rendering a template out like this and making use of conditionals in a nice way
-echo "server {";
-echo "    listen 80;";
-echo "    index index.php index.html;";
-echo "    error_log  /var/log/nginx/error.log;";
-echo "    access_log /var/log/nginx/access.log;";
-echo "    root /var/www/${DOCUMENT_ROOT};";
-echo "";
+# Start server section
+envsubst <<EOF
+server {
+    listen 80;
+    index index.php index.html;
+    error_log /var/log/nginx/error.log;
+    access_log /var/log/nginx/access.log;
+    root /var/www/${DOCUMENT_ROOT};
+EOF
 if [[ "$CORS_ENABLED" == "1" ]]; then
-echo "    add_header 'Access-Control-Allow-Origin' '*' always;";
-echo "    add_header 'Access-Control-Allow-Methods' 'GET, POST, PUT, PATCH, OPTIONS, DELETE';";
-echo "    add_header 'Access-Control-Allow-Headers' 'DNT,Origin,X-Device-UUID,X-Device-Version,X-Device-PLatform,X-Device-Model,X-Auth-Token,X-Requested-With,Content-Type,If-Modified-Since,User-Agent,Keep-Alive,Content-Length,Accept,Cache-Control,X-Upload-Content-Length,X-Upload-Content-Type,Authorization';";
-echo "    add_header 'Access-Control-Expose-Headers' 'X-Location';";
-echo "";
+cat <<EOF
+    add_header 'Access-Control-Allow-Origin' '*' always;
+    add_header 'Access-Control-Allow-Methods' 'GET, POST, PUT, PATCH, OPTIONS, DELETE';
+    add_header 'Access-Control-Allow-Headers' 'DNT,Origin,X-Device-UUID,X-Device-Version,X-Device-PLatform,X-Device-Model,X-Auth-Token,X-Requested-With,Content-Type,If-Modified-Since,User-Agent,Keep-Alive,Content-Length,Accept,Cache-Control,X-Upload-Content-Length,X-Upload-Content-Type,Authorization';
+    add_header 'Access-Control-Expose-Headers' 'X-Location';
+EOF
 fi
-echo "    location ~ \.php$ {";
+
+# Location ~ \.php files
+cat <<EOF
+
+    location ~ \.php$ {
+        try_files \$uri =404;
+        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_pass app:9000;
+        fastcgi_index index.php;
+        include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        fastcgi_param PATH_INFO \$fastcgi_path_info;
+EOF
 if [[ "$CORS_ENABLED" == "1" ]]; then
-echo "        add_header 'Access-Control-Allow-Origin' '*' always;";
-echo "        add_header 'Access-Control-Allow-Methods' 'GET, POST, PUT, PATCH, OPTIONS, DELETE';";
-echo "        add_header 'Access-Control-Allow-Headers' 'DNT,Origin,X-Device-UUID,X-Device-Version,X-Device-PLatform,X-Device-Model,X-Auth-Token,X-Requested-With,Content-Type,If-Modified-Since,User-Agent,Keep-Alive,Content-Length,Accept,Cache-Control,X-Upload-Content-Length,X-Upload-Content-Type,Authorization';";
-echo "        add_header 'Access-Control-Expose-Headers' 'X-Location';";
-echo "";
+cat <<EOF
+        add_header 'Access-Control-Allow-Origin' '*' always;
+        add_header 'Access-Control-Allow-Methods' 'GET, POST, PUT, PATCH, OPTIONS, DELETE';
+        add_header 'Access-Control-Allow-Headers' 'DNT,Origin,X-Device-UUID,X-Device-Version,X-Device-PLatform,X-Device-Model,X-Auth-Token,X-Requested-With,Content-Type,If-Modified-Since,User-Agent,Keep-Alive,Content-Length,Accept,Cache-Control,X-Upload-Content-Length,X-Upload-Content-Type,Authorization';
+        add_header 'Access-Control-Expose-Headers' 'X-Location';
+EOF
 fi
-echo "        try_files \$uri =404;";
-echo "        fastcgi_split_path_info ^(.+\.php)(/.+)$;";
-echo "        fastcgi_pass app:9000;";
-echo "        fastcgi_index index.php;";
-echo "        include fastcgi_params;";
-echo "        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;";
-echo "        fastcgi_param PATH_INFO \$fastcgi_path_info;";
-if [[ ! -z "${SHARED_URL}" ]]; then
-echo "        sub_filter ${APP_URL} ${SHARED_URL};";
-echo "        sub_filter_once off;";
+if [[ -n "${SHARED_URL}" ]]; then
+envsubst <<EOF
+        sub_filter ${APP_URL} ${SHARED_URL};";
+        sub_filter_once off;";
+EOF
 fi
-echo "    }";
-echo "";
-echo "    location / {";
+cat <<EOF
+    }
+EOF
+
+# Location / files
+cat <<EOF
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;";
+        gzip_static on;";
+EOF
 if [[ "$CORS_ENABLED" == "1" ]]; then
-echo "    if (\$request_method = 'OPTIONS') {";
-echo "        # Tell client that this pre-flight info is valid for 20 days";
-echo "        add_header 'Access-Control-Allow-Origin' '*';";
-echo "        add_header 'Access-Control-Allow-Methods' 'GET, POST, PUT, PATCH, OPTIONS, DELETE';";
-echo "        add_header 'Access-Control-Allow-Headers' 'DNT,Origin,X-Auth-Token,X-Device-UUID,X-Device-Version,X-Device-PLatform,X-Device-Model,X-Requested-With,Content-Type,If-Modified-Since,User-Agent,Keep-Alive,Content-Length,Accept,Cache-Control,X-Upload-Content-Length,X-Upload-Content-Type,Authorization';";
-echo "        add_header 'Access-Control-Expose-Headers' 'X-Location';";
-echo "        # add_header 'Access-Control-Max-Age' 1728000;";
-echo "        add_header 'Content-Type' 'text/plain charset=UTF-8';";
-echo "        add_header 'Content-Length' 0;";
-echo "        return 204;";
-echo "    }";
-echo "";
+cat <<EOF
+        if (\$request_method = 'OPTIONS') {
+            # Tell client that this pre-flight info is valid for 20 days
+            add_header 'Access-Control-Allow-Origin' '*';
+            add_header 'Access-Control-Allow-Methods' 'GET, POST, PUT, PATCH, OPTIONS, DELETE';
+            add_header 'Access-Control-Allow-Headers' 'DNT,Origin,X-Auth-Token,X-Device-UUID,X-Device-Version,X-Device-PLatform,X-Device-Model,X-Requested-With,Content-Type,If-Modified-Since,User-Agent,Keep-Alive,Content-Length,Accept,Cache-Control,X-Upload-Content-Length,X-Upload-Content-Type,Authorization';
+            add_header 'Access-Control-Expose-Headers' 'X-Location';
+            # add_header 'Access-Control-Max-Age' 1728000;
+            add_header 'Content-Type' 'text/plain charset=UTF-8';
+            add_header 'Content-Length' 0;
+            return 204;
+        }
+EOF
 fi
-echo "        try_files \$uri \$uri/ /index.php?\$query_string;";
-echo "        gzip_static on;";
-if [[ ! -z "${SHARED_URL}" ]]; then
-echo "        sub_filter ${APP_URL} ${SHARED_URL};";
-echo "        sub_filter_once off;";
+if [[ -n "${SHARED_URL}" ]]; then
+envsubst <<EOF
+        sub_filter ${APP_URL} ${SHARED_URL};";
+        sub_filter_once off;";
+EOF
 fi
-echo "    }";
-echo "}";
+cat <<EOF
+    }
+EOF
+
+# End server section
+cat <<EOF
+}
+EOF
