73019aa9a9dfdce1c8439f55eaf96c3bd59b75ed
Tom Harper <tom.harper@rareloop.com>
2022-06-28 09:22:42 +0100

    Copy .env.qa for wordpress correctly

--- resources/scripts/php/root-start-script.sh
+++ resources/scripts/php/root-start-script.sh
@@ -10,6 +10,7 @@ COMPOSER_BRANCH="${COMPOSER_BRANCH:=${DEFAULT_COMPOSER_BRANCH}}"
 PROJECT_TYPE='php';
 XDEBUG_ENABLED="${XDEBUG_ENABLED:=0}"
 XDEBUG_PROFILER_ENABLED="${XDEBUG_PROFILER_ENABLED:=0}"
+VOLKER_PROJECT_TYPE="${VOLKER_PROJECT_TYPE:='laravel'}"
 
 if [ -f 'volker.json' ]; then
     PROJECT_TYPE=$(grep 'type' volker.json | sed "s/.*project_type\": \"\([a-zA-Z]*\)\".*/\1/g");
@@ -63,7 +64,14 @@ if [[ ! -z "${QA_INSTANCE}" ]]; then
         echo "Restoring QA Instance: \"${QA_INSTANCE}\""
         sudo tar -xf /qa/${QA_INSTANCE}.tar -C /
 
-        cp /var/www/.env.qa /var/www/.env 
+        if [[ "$VOLKER_PROJECT_TYPE" == "laravel" ]]; then
+            cp /var/www/.env.qa /var/www/.env 
+        fi
+
+        if [[ "$VOLKER_PROJECT_TYPE" == "wordpress" ]]; then
+            cp /var/www/site/.env.qa /var/www/site/.env 
+        fi
+        
         echo 'Restored'
     fi
 fi
