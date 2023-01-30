92da5932854b63252ae7cbcbf4524b8fc5a0e9a0
Tom Harper <tom.harper@rareloop.com>
2023-01-30 13:05:13 +0000

    NVM version should be configurable

--- resources/scripts/php/install_nvm.sh
+++ resources/scripts/php/install_nvm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p $NVM_DIR
-sudo -H -u www bash -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash"
+sudo -H -u www bash -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_VERSION/install.sh | bash"
