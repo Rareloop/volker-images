7e78d8d67f455a25e9aaf982d5d7ca9e3086a916
Ollie Wells <ollie.wells@rareloop.com>
2024-02-20 10:41:52 +0000

    Update run scheduler to be configurable

--- resources/scripts/php/run-scheduler.sh
+++ resources/scripts/php/run-scheduler.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
+
+# Get the value of the ENV_VAR environment variable or use a default value
+SCHEDULE_MINUTES=${INTERVAL_MINUTES}
+COMMAND=${CRON_COMMAND}
+
+if [[ "" == "${COMMAND}" ]]; then
+    # Default to the laravel command for backward compatibility
+    COMMAND="php /var/www/artisan schedule:run --verbose --no-interaction"
+fi
+
+if [[ "" == "${SCHEDULE_MINUTES}" ]]; then
+    # Default to every minute
+    SCHEDULE_MINUTES=1
+fi
+
+# Calculate seconds
+SCHEDULE_SECONDS=$((SCHEDULE_MINUTES * 1))
+
+echo "Running every ${SCHEDULE_MINUTES} minutes: ${COMMAND}"
+
 while true
 do
-  php /var/www/artisan schedule:run --verbose --no-interaction &
-  sleep 60
+  ${COMMAND} &
+  sleep ${SCHEDULE_SECONDS}
 done
